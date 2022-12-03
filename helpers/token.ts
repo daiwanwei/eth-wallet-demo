f7790e44beb8579f98ecd890e1f7b94df2589b22
daiwanwei <wadejet.work@gmail.com>
2022-12-03 18:39:05 +0800

    fix image uri

--- helpers/token.ts
+++ helpers/token.ts
@@ -17,13 +17,14 @@ export async function getHeldTokens(
         {contractAddresses:whitelistContracts}
     )
     const {ownedNfts} =results
-    const data=ownedNfts.map(({contract,tokenId,rawMetadata,balance})=>{
+    const data=ownedNfts.map(({contract,tokenId,media,balance})=>{
         const {address,name,symbol }=contract
+        const image=media.length>=0? media[0].gateway: ""
         return {
             contractAddress:address,
             name:name || "",
             symbol:symbol || "",
-            image:rawMetadata?.image || "",
+            image,
             tokenId,
             amount:balance
         }
